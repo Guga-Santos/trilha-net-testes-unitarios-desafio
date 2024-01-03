81e603f585955c78cbd8c7145aacd577fe3f1fb7
Guga <guga@Homme.Guga>
2024-01-03 15:46:31 -0300

    fix: ultimo teste de listas

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -50,6 +50,7 @@ public class ValidacoesListaTests
     }
 
     //TODO: Corrigir a anotação [Fact]
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
